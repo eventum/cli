311c58d9922d8c965ab3cca19c44629880e0be6d
Elan Ruusamäe <glen@pld-linux.org>
2017-09-06 20:57:19 +0300

    use padraic/phar-updater for self-update
    herrera-io/phar-update is marked as abandoned

--- src/Command/SelfUpdateCommand.php
+++ src/Command/SelfUpdateCommand.php
@@ -15,13 +15,22 @@ namespace Eventum\Console\Command;
 
 use Herrera\Phar\Update\Manager;
 use Herrera\Phar\Update\Manifest;
+use Humbug\SelfUpdate\Strategy\GithubStrategy;
+use Humbug\SelfUpdate\Strategy\ShaStrategy;
 use KevinGH\Version\Version;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Humbug\SelfUpdate\Updater;
 
 class SelfUpdateCommand extends Command
 {
-    const MANIFEST_FILE = 'https://raw.githubusercontent.com/eventum/cli/dist/manifest.json';
+    /** for github strategy: stable releases */
+    const PackageName = 'eventum/cli';
+    const PharFile = 'eventum.phar';
+
+    /** for sha1 strategy: snapshot releases */
+    const PharFileUrl = 'https://raw.githubusercontent.com/eventum/cli/dist/eventum.phar';
+    const VersionFileUrl = 'https://raw.githubusercontent.com/eventum/cli/dist/versions.json';
 
     protected function configure()
     {
@@ -32,15 +41,50 @@ class SelfUpdateCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $manager = new Manager(Manifest::loadFile(self::MANIFEST_FILE));
-        $version = Version::create($this->getApplication()->getVersion());
+        $updater = $this->getUpdater();
 
-        $update = $manager->getManifest()->findRecent($version, true, true);
-        if ($update) {
-            $output->writeln("<info>Updating to version {$update->getVersion()}.</info>");
-            $manager->update($version, true, true);
-        } else {
-            $output->writeln("<info>You are already using composer version {$version}.</info>");
+        $result = $updater->update();
+        if (!$result) {
+            $version = $updater->getNewVersion();
+            $output->writeln("<info>You are already using version {$version}.</info>");
+            return 0;
         }
+
+        $new = $updater->getNewVersion();
+        $old = $updater->getOldVersion();
+        $output->writeln("<info>Updated from {$old} to {$new}.</info>");
+
+        return 0;
+    }
+
+    /**
+     * Get updater based whether we are on stable or unstable channel.
+     *
+     * @return Updater
+     */
+    private function getUpdater()
+    {
+        $appVersion = $this->getApplication()->getVersion();
+        $version = Version::create($appVersion);
+
+        $updater = new Updater(null, false);
+
+        if ($version->isStable()) {
+            $updater->setStrategy(Updater::STRATEGY_GITHUB);
+
+            /** @var GithubStrategy $strategy */
+            $strategy = $updater->getStrategy();
+
+            $strategy->setPackageName(self::PackageName);
+            $strategy->setPharName(self::PharFile);
+            $strategy->setCurrentLocalVersion($appVersion);
+        } else {
+            /** @var ShaStrategy $strategy */
+            $strategy = $updater->getStrategy();
+            $strategy->setPharUrl(self::PharFileUrl);
+            $strategy->setVersionUrl(self::VersionFileUrl);
+        }
+
+        return $updater;
     }
 }
